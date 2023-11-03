732348bf9b8d0b5ad9b23e468132ea8e95f2b77c
AlienIdeology <wcliaw610@gmail.com>
2023-11-03 16:59:34 -0500

    init

---
+++ tests/app/index.js
@@ -0,0 +1,7 @@
+// For testing by adding the library into dependencies:
+// "express-coercer-validator": "file: ../../../../express-coercer-validator-0.0.1.tgz"
+
+// import lib from "express-coercer-validator"
+const lib = require("express-coercer-validator")
+
+lib.default()
